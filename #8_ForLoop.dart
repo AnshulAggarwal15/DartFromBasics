33ee9163f86372cfd9809c1a63b64f98156786b4
AnshulAggarwal15 <43234382+AnshulAggarwal15@users.noreply.github.com>
2020-01-04 00:11:13 +0530

    Create #8_ForLoop.dart

---
+++ #8_ForLoop.dart
@@ -0,0 +1,27 @@
+void main() {
+
+	// FOR Loop
+  
+	// WAP to find the all numbers between 1 to 100
+  
+  for (int i = 1; i <= 100 i++) {
+      	print(i);
+		}
+
+	// WAP to find the odd numbers between 1 to 100
+
+	for (int i = 1; i <= 100;i++) {
+
+		if ( i % 2 != 0) {
+			print(i);
+		}
+	}
+  
+  // WAP to find the even numbers between 1 to 100
+
+	for (int i = 1; i <= 100;i++) {
+
+		if ( i % 2 == 0) {
+			print(i);
+		}
+	}
