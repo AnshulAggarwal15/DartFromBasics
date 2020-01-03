f7ae3ac9aa61af78bf976245400f1770e85c83fc
AnshulAggarwal15 <43234382+AnshulAggarwal15@users.noreply.github.com>
2020-01-04 00:19:02 +0530

    Create #10_LoopInList.dart

---
+++ #10_LoopInList.dart
@@ -0,0 +1,13 @@
+void main() {
+
+	// for loop in List
+  // List is a type of Data Structure-an organised way to store data
+  
+	List planetList = ["Mercury", "Venus", "Earth", "Mars"];
+
+  for (String planet in planetList) {
+		// In above statement "String planet" is similar to int i in last git file
+    print(planet);
+	}
+
+}
