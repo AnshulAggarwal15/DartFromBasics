2d7c37e3c40a5804eb147493e970b77004a736ea
AnshulAggarwal15 <43234382+AnshulAggarwal15@users.noreply.github.com>
2020-01-05 00:43:28 +0530

    Create #11_WhileLoop.dart

---
+++ #11_WhileLoop.dart
@@ -0,0 +1,30 @@
+void main() {
+
+	// FOR Loop
+  
+	// WAP to find the all numbers between 1 to 100
+  
+  int i=0;
+  
+  while(i<=100) {
+      	print(i);
+		}
+
+	// WAP to find the odd numbers between 1 to 100
+
+	while(i<=100) {
+
+		if ( i % 2 != 0) {
+			print(i);
+		}
+	}
+  
+  // WAP to find the even numbers between 1 to 100
+
+	while(i<=100) {
+
+		if ( i % 2 == 0) {
+			print(i);
+		}
+	 }
+  }
