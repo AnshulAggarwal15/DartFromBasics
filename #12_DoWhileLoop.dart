6718cf75f23b80c20159407ae5add09e488bb2be
AnshulAggarwal15 <43234382+AnshulAggarwal15@users.noreply.github.com>
2020-01-05 00:47:18 +0530

    Create #12_DoWhileLoop.dart

---
+++ #12_DoWhileLoop.dart
@@ -0,0 +1,27 @@
+void main() {
+
+	// DO-WHILE Loop
+	// WAP to find the even numbers between 1 to 10
+
+	int i = 1;
+
+	do {
+
+		if ( i % 2 == 0) {
+			print(i);
+		}
+
+		i++;
+	} while ( i <= 10);
+  
+ // WAP to find the odd numbers between 1 to 10 
+  do {
+
+		if ( i % 2 != 0) {
+			print(i);
+		}
+
+		i++;
+	} while ( i <= 10);
+
+}
