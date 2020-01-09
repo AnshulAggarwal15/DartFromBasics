764494a9bf82663e7b4c6d8ecc3e1822f2928551
AnshulAggarwal15 <43234382+AnshulAggarwal15@users.noreply.github.com>
2020-01-10 00:02:29 +0530

    Readme edits (#10)
    * Create #5_FinalvsConst.dart
    
    * Create #6_IfElseLoop.dart
    
    * Create #7_conditional_expressions.dart
    
    * Create #8_SwitchCase.dart
    
    * Create #8_ForLoop.dart
    
    * Rename #8_ForLoop.dart to #9_ForLoop.dart
    
    * Create #10_LoopInList.dart
    
    * Create #11_WhileLoop.dart
    
    * Create #12_DoWhileLoop.dart
    
    * Create #13_BreakKeyword.dart
    
    * Create 14_continue_keyword.dart
    
    * Create #15_Functions.dart
    
    * Create #16_FunctionExpressions.dart
    
    * Create #17_Parameters.dart
    
    * Create #18_NamedParameters.dart

---
+++ #17_Parameters.dart
@@ -0,0 +1,26 @@
+void main() {
+
+	printCities("New York", "New Delhi", "Sydney");
+	print("");
+
+	printCountries("USA");  // You can skip the Optional Positional Parameters
+
+}
+
+// Required Parameters
+void printCities(String name1, String name2, String name3) {
+
+	print("Name 1 is $name1");
+	print("Name 2 is $name2");
+	print("Name 3 is $name3");
+}
+
+// Optional Positional Parameters
+void printCountries(String name1, [String name2, String name3]) {
+    // parameters in "[]" represents optional parameters
+    // optional parameters are given value == NULL
+    
+	print("Name 1 is $name1");
+	print("Name 2 is $name2");
+	print("Name 3 is $name3");
+}
