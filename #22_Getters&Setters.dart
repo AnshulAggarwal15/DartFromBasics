b35223d2072ed2ea18e3931ee1c74b5fcc1cf732
AnshulAggarwal15 <43234382+AnshulAggarwal15@users.noreply.github.com>
2020-01-14 23:50:56 +0530

    Create #22_Getters&Setters.dart

---
+++ #22_Getters&Setters.dart
@@ -0,0 +1,21 @@
+void main() {
+
+  var student = Student();
+  student.name = "Peter";     // Calling default Setter to set value
+  print(student.name);        // Calling default Getter to get value
+
+  student.percentage = 438.0;   // Calling Custom Setter to set value
+  print(student.percentage);    // Calling Custom Getter to get value
+}
+
+class Student {
+
+  String name;        // Instance Variable with default Getter and Setter
+
+  double _percent;    // Private Instance Variable for its own library
+
+  // Instance variable with Custom Setter
+  void set percentage(double marksSecured) => _percent = (marksSecured / 500) * 100;
+  // Instance variable with Custom Getter
+  double get percentage => _percent;
+}
