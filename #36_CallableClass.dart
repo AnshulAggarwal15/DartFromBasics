3ac3c8c15ec8132dcecb58a88de1d14d42f01244
AnshulAggarwal15 <43234382+AnshulAggarwal15@users.noreply.github.com>
2020-02-01 14:16:10 +0530

    Create #36_CallableClass.dart

---
+++ #36_CallableClass.dart
@@ -0,0 +1,13 @@
+void main() {
+
+	var personOne = Person();
+	var msg = personOne(25, "Peter");
+	print(msg);
+}
+
+class Person {
+	
+	String call(int age, String name) {
+		return "The name of the person is $name and age is $age";
+	}
+}
